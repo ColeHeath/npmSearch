a6a0d1182c11459bbbb76de7cb6247c9a225345d
Cole Heath <coleheath@Tais-MacBook-Pro.local>
2017-05-30 17:12:18 -0500

    updated npmSearch.js

--- npmSearch.js
+++ npmSearch.js
@@ -11,6 +11,11 @@ module.exports = {
       .setValue('input[type=search]',['node',browser.Keys.ENTER])
       .pause(1000)
       .assert.containsText('[data-reactid="94"]','NODE')
+      .click('[data-reactid="9"]')
+      .pause(1000)
+      .setValue('input[type=search]',['not here',browser.Keys.ENTER])
+      .pause(1000)
+      .assert.containsText('[data-reactid="94"]','not here')
       .end()
   }
 }
