8d2fdae0c0638c8ec5a70a6482d1872d6fef11f9
Cole Heath <coleheath@Tais-MacBook-Pro.local>
2017-05-30 17:07:40 -0500

    second commit

---
+++ npmSearch.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'npmSearch': function (browser) {
+    browser
+      .url('https://www.npmjs.com/')
+      .pause(1000)
+      .setValue('input[type=search]',['eslint',browser.Keys.ENTER])
+      .pause(1000)
+      .assert.containsText('[data-reactid="94"]','eslint')
+      .click('[data-reactid="9"]')
+      .pause(1000)
+      .setValue('input[type=search]',['node',browser.Keys.ENTER])
+      .pause(1000)
+      .assert.containsText('[data-reactid="94"]','NODE')
+      .end()
+  }
+}
