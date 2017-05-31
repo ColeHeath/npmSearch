7ba9ca2d35eebdfffb3b943eb3c91aa8771de547
Cole Heath <coleheath@Tais-MacBook-Pro.local>
2017-05-31 14:06:51 -0500

    update for Page Objects

---
+++ path/example.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const commands = {
+  isLoaded (browser) {
+    return this
+      .waitForElementVisible('@loadElement', 10000);
+  },
+  search (text) {
+    return this
+      .setValue('input[type=search]',[text])
+      //browser.sendKeys('input[type=search]',browser.Keys.ENTER)
+  },
+  searchEnter () {
+    return this
+      .click('[tabindex="2"]')
+  }
+};
+
+module.exports = {
+  url () {
+    return 'https://npmjs.org';
+  },
+  commands: [commands],
+  elements: {
+    equipmentTable: '.equipments_table div:nth-child(2) table:nth-child(1)',
+    data_id: '[data-reactid="94"]',
+    search_id: 'input[type=search]',
+    fail_id: '[data-reactid="83"]',
+    loadElement: '.npm-loves-you'
+  }
+};
